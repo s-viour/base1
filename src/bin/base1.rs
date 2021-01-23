9768476d9334bcfb58ffc1fec370635ea66c110e
matt wyatt <mwyatt1000@gmail.com>
2021-01-22 20:04:10 -0500

    added a more size-efficient encode option, encodelong. and added a size option to get the size of the file that would result from encoding

--- src/bin/base1.rs
+++ src/bin/base1.rs
@@ -1,21 +1,34 @@
-use std::io::{self, Read, Write};
+use std::io::{self, Read, Write, BufWriter};
 use std::env;
 use base1;
 
 fn main() -> io::Result<()> {
 	let args: Vec<String> = env::args().collect();
-	if args.len() != 2 || args[1] != "encode" && args[1] != "decode" {
-		println!("usage: {} [encode|decode]", args[0]);
+	if args.len() != 2 {
+		println!("usage: {} [encode|decode|encodelong|size]", args[0]);
 	} else {
 		let mut stdin = io::stdin();
 		let mut stdout = io::stdout();
+		let arg = args[1].as_str();
 		let mut s = String::new();
 		stdin.read_to_string(&mut s)?;
 
-		if args[1] == "encode" {
+		if arg == "encode" {
 			print!("{}", base1::encode(&s.trim().as_bytes().to_vec()));
-		} else {
+		} else if arg == "decode" {
 			stdout.write(&base1::decode(&s.trim().to_string()))?;
+		} else if arg == "encodelong" {
+			let l = base1::encode_l(&s.trim().as_bytes().to_vec());
+			let mut buffer = BufWriter::new(stdout);
+			for c in base1::Base1Producer::new(l) {
+				buffer.write(&[c as u8]).unwrap();
+			}
+		} else if arg == "size" {
+			let num = base1::encode_l(&s.trim().as_bytes().to_vec());
+			println!("{}", num)
+
+		} else {
+			println!("unknown command {}", arg);
 		}
 	}
 	Ok(())
